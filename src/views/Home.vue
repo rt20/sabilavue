b323aaac0868563115aab28ebc0096c52f47dfca
hilman <kaploks@gmail.com>
2020-03-01 10:07:42 +0700

    upload project vue

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="home">
+  <ShaynaHeader/>
+   <HeroSection/>
+   <WomenShayna/>
+   <InstaShayna/>
+   <PartnerShayna/>
+   <FooterShayna/>
+  </div>
+</template>
+
+<script>
+import ShaynaHeader from "@/components/ShaynaHeader.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import WomenShayna from "@/components/WomenShayna.vue";
+import InstaShayna from "@/components/InstaShayna.vue";
+import PartnerShayna from "@/components/PartnerShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+// import carousel from 'vue-owl-carousel';
+
+
+
+export default {
+  name: 'home',
+  components:{
+      ShaynaHeader,
+      HeroSection,
+      WomenShayna,
+      InstaShayna,
+      PartnerShayna,
+      FooterShayna
+    //   carousel
+      
+  }
+};
+</script>
+
