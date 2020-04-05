cfce280c557da0a2467e71529213ecf8bda6c938
hilman <kaploks@gmail.com>
2020-04-05 12:53:29 +0700

    add revisi API sandangan ke-2

--- src/components/ShaynaHeader.vue
+++ src/components/ShaynaHeader.vue
@@ -30,49 +30,43 @@
                                 Keranjang Belanja &nbsp;
                                 <a href="#">
                                     <i class="icon_bag_alt"></i>
-                                    <span>3</span>
+                                    <span>{{ keranjangUser.length}}</span>
                                 </a>
                                 <div class="cart-hover">
                                     <div class="select-items">
                                         <table>
-                                            <tbody>
-                                                <tr>
+                                            <tbody v-if="keranjangUser.length > 0">
+                                                <tr v-for="keranjang in keranjangUser" :key="keranjang.id">
                                                     <td class="si-pic">
-                                                        <img src="img/select-product-1.jpg" alt="" />
+                                                        <img class="photo-item" :src="keranjang.photo" alt="" />
                                                     </td>
                                                     <td class="si-text">
                                                         <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
+                                                            <p>Rp {{ keranjang.price}}</p>
+                                                            <h6>{{ keranjang.name}}</h6>
                                                         </div>
                                                     </td>
-                                                    <td class="si-close">
+                                                    <td @click="removeItem(keranjangUser.index)" class="si-close">
                                                         <i class="ti-close"></i>
                                                     </td>
                                                 </tr>
+                                               
+                                            </tbody>
+                                            <tbody v-else>
                                                 <tr>
-                                                    <td class="si-pic">
-                                                        <img src="img/select-product-2.jpg" alt="" />
-                                                    </td>
-                                                    <td class="si-text">
-                                                        <div class="product-selected">
-                                                            <p>$60.00 x 1</p>
-                                                            <h6>Kabino Bedside Table</h6>
-                                                        </div>
-                                                    </td>
-                                                    <td class="si-close">
-                                                        <i class="ti-close"></i>
-                                                    </td>
+                                                    <td>Keranjang Kosong</td>
                                                 </tr>
                                             </tbody>
                                         </table>
                                     </div>
                                     <div class="select-total">
                                         <span>total:</span>
-                                        <h5>$120.00</h5>
+                                        <h5>Rp {{ totalHarga }}.00</h5>
                                     </div>
                                     <div class="select-button">
-                                        <a href="#" class="primary-btn view-card">VIEW CARD</a>
+                                        
+                                        <a href="#" class="primary-btn view-card"> <router-link to="/cart" style="color: #FFF;" >VIEW CARD  </router-link></a>
+                                       
                                         <a href="#" class="primary-btn checkout-btn">CHECK OUT</a>
                                     </div>
                                 </div>
@@ -89,7 +83,44 @@
 
 <script>
 export default {
-  name: 'ShaynaHeader'
+  name: 'ShaynaHeader',
+  data(){
+    return{
+        keranjangUser: [] 
+    };
+},
+methods: {
+    removeItem(index){
+        this.keranjangUser.splice(index, 1);
+        const parsed = JSON.stringify(this.keranjangUser);
+        localStorage.setItem('keranjangUser', parsed);
+    }
+},
+mounted(){
+      if (localStorage.getItem('keranjangUser')) {
+      try {
+        this.keranjangUser = JSON.parse(localStorage.getItem('keranjangUser'));
+      } catch(e) {
+        localStorage.removeItem('keranjangUser');
+      }
+    }
+},
   
+
+computed: {
+    totalHarga(){
+        return this.keranjangUser.reduce(function(items, data){
+            return items + data.price;
+        }, 0);
+    }
 }
+};
 </script>
+
+<style scoped>
+.photo-item {
+    width: 80px;
+    height: 80px;
+}
+
+</style>
--- src/components/WomenShayna.vue
+++ src/components/WomenShayna.vue
@@ -3,28 +3,29 @@
     <section class="women-banner spad">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-12 mt-5">
+                <div class="col-lg-12 mt-5" v-if="products.length > 0">
                     <carousel class="product-slider" :item="3" :nav="false" :autoplay="true" :dots="false">
-                        <div class="product-item">
+                       
+                        <div class="product-item" v-for="itemProduct in products" v-bind:key="itemProduct.id">
                             <div class="pi-pic">
-                                <img src="img/mickey1.jpg" alt="" />
+                                <img v-bind:src="itemProduct.galleries[0].photo" alt="" />
                                 <ul>
-                                    <li class="w-icon active">
+                                    <li @click="saveKeranjang(itemProduct.id, itemProduct.name, itemProduct.price, itemProduct.galleries[0].photo)" class="w-icon active">
                                         <a href="#"><i class="icon_bag_alt"></i></a>
                                     </li>
                                     <li class="quick-view">
-                                        <router-link to="/product">+ Quick View</router-link>
+                                        <router-link v-bind:to="'/product/'+itemProduct.id">+ Quick View</router-link>
                                         </li>
                                 </ul>
                             </div>
                             <div class="pi-text">
-                                <div class="catagory-name">Coat</div>
+                                <div class="catagory-name">{{ itemProduct.type}}</div>
                                 <router-link to="/product">
                                 <a href="#">
-                                    <h5>Mickey Baggy</h5>
+                                    <h5>{{ itemProduct.name}}</h5>
                                 </a></router-link>
                                 <div class="product-price">
-                                    $14.00
+                                    Rp{{ itemProduct.price}}
                                     <span>$35.00</span>
                                 </div>
                             </div>
@@ -94,6 +95,16 @@
                         </div>
                     </carousel>
                 </div>
+
+            <div class="col-lg-12" v-else>
+                <p>
+                    Data Tidak Ditemukan
+                </p>
+
+            </div>
+
+
+
             </div>
         </div>
     </section>
@@ -102,12 +113,41 @@
 </template>
 <script>
 import carousel from "vue-owl-carousel";
-
+import axios from "axios";
 export default {
   name: 'WomenShayna',
   components:{
      carousel
- }
+ },
+ data(){
+     return {
+         products: []
+     };
+ },
+
+ methods:{
+   
+    saveKeranjang(idProduct, nameProduct, priceProduct, photoProduct) {
+
+        var productStored = {
+            "id" : idProduct,
+            "name" : nameProduct,
+            "price" : priceProduct,
+            "photo" : photoProduct
+        };
+         this.keranjangUser.push(productStored);
+         const parsed = JSON.stringify(this.keranjangUser);
+        localStorage.setItem('keranjangUser', parsed);
+    }
+},
+mounted(){
+    axios
+        .get("http://shayna-backend.belajarkoding.com/api/products")
+        .then(res => (this.products = res.data.data.data))
+        .catch(err => console.log(err));
+}
+
+
 };
 </script>
 <style scoped>
