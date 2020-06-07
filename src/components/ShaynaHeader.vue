00bce09e396f10e683770cea4c22c33c8ce79746
rt20 <kaploks@gmail.com>
2020-06-07 21:57:04 +0700

    add contact

--- src/components/ShaynaHeader.vue
+++ src/components/ShaynaHeader.vue
@@ -5,10 +5,10 @@
             <div class="container">
                 <div class="ht-left">
                     <div class="mail-service">
-                        <i class=" fa fa-envelope"></i> hello.shayna@gmail.com
+                        <i class=" fa fa-envelope"></i> mybalokid@gmail.com
                     </div>
                     <div class="phone-service">
-                        <i class=" fa fa-phone"></i> +628 22081996
+                        <i class=" fa fa-phone"></i> +628 123 0000 91
                     </div>
                 </div>
             </div>
